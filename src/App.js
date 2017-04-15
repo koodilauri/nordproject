5256f33ef99abff982414935d0d0439edfc20e1d
Lauri Koivisto <fuksi@dhcp-asv-75.helsinki.fi>
2017-04-15 14:05:02 +0300

    created basic structure

--- src/App.js
+++ src/App.js
@@ -2,17 +2,85 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function generateUserValues() {
+  let users = []
+  for (let i =0;i<20;i++){
+    users.push({
+      id: i,
+      name: "a"+i,
+      email: "a"+i+"@a.fi",
+      phone: "00"+i
+    })
+  }
+  return users;
+}
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      users: generateUserValues()
+    }
+  }
+
+  renderCreateUser(){
+    return(
+      <form>
+        <div>
+          <div>
+            <label>Name</label>
+            <input/>
+          </div>
+          <div>
+            <label>E-mail</label>
+            <input/>
+          </div>          
+          <div>
+            <label>Phone</label>
+            <input/>
+          </div>
+        </div>
+        <button type="submit">Add new</button>
+      </form>
+    )
+  }
+
+  renderUserList() {
+    // const users = this.state.users;
+    const { users } = this.state;
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>E-mail</td>
+              <td>Phone</td>
+            </tr>
+          </thead>
+          <tbody>
+            { users.map(user => 
+           <tr>
+            <td>{ user.name }</td>
+            <td>{ user.email }</td>
+            <td>{ user.phone }</td>            
+          </tr>
+        )}
+          </tbody>
+        </table>
+
+      </div>
+    )
+  }
+
   render() {
+    console.log(this.state.users)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>asdf</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>List of participants</h1>
+        {this.renderCreateUser()}
+        {this.renderUserList()}
       </div>
     );
   }
